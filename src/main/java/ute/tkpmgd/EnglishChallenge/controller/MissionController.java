098698158eda3b1e8ffaca470f03979f8752cf9d
TrungbadaoQN <15110346@student.hcmute.edu.vn>
2019-05-05 14:55:41 +0700

    không biết ghi gì hết

---
+++ src/main/java/ute/tkpmgd/EnglishChallenge/controller/MissionController.java
@@ -0,0 +1,10 @@
+package ute.tkpmgd.EnglishChallenge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mission")
+public class MissionController {
+	
+}
