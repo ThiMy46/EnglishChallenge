f61a0bd6c8df821d0c065dc4e1ca5548a1c77f12
ThiMy46 <31314956+ThiMy46@users.noreply.github.com>
2019-05-05 14:31:41 +0700

    update model v2
    update: AUTO -> IDENTITY, onetomany of user-userinfo, String->boolean of typeMission field in Mission model

--- src/main/java/ute/tkpmgd/EnglishChallenge/model/Challenge.java
+++ src/main/java/ute/tkpmgd/EnglishChallenge/model/Challenge.java
@@ -15,7 +15,7 @@ public class Challenge implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idChallenge;
 
 	private String createTime;
--- src/main/java/ute/tkpmgd/EnglishChallenge/model/Question.java
+++ src/main/java/ute/tkpmgd/EnglishChallenge/model/Question.java
@@ -15,7 +15,7 @@ public class Question implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idQuestion;
 
 	@Lob
